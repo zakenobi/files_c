3962bea3eabebf4eb310a9044c0aa79a2c4731c0
zakenobi <vanvansurferman@gmail.com>
2019-09-04 23:13:42 +0200

    base files

---
+++ count.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int funct(int x, int y)
+{
+    int rslt;
+    rslt=x*y;
+  return rslt;
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "count.h"
+
+int result;
+int main()
+{
+    result= funct(4,2);
+    printf("%d",result);
+}
